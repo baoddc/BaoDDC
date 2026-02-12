d26c47fe2add7658f080a94fd4717991dafd936c
BaoDDC <thaibao06061997@gmail.com>
2026-02-13 02:16:51 +0700

    Update home.js

--- assets/js/home.js
+++ assets/js/home.js
@@ -2,7 +2,7 @@
 window.addEventListener('load', () => {
   const currentUser = localStorage.getItem('currentUser');
   if (!currentUser) {
-    window.location.href = 'dang_nhap.html';
+    window.location.href = 'index.html';
     return;
   }
   
@@ -15,7 +15,7 @@ window.addEventListener('load', () => {
   if (btnLogout) {
     btnLogout.addEventListener('click', () => {
       localStorage.removeItem('currentUser');
-      window.location.href = 'dang_nhap.html';
+      window.location.href = 'index.html';
     });
   }
 });
