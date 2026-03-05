a21f7197b23d839fd99f92406c72b71aa729b5bb
BaoDDC <thaibao06061997@gmail.com>
2026-03-05 16:18:36 +0700

    Update tole-nhap.js

--- assets/js/tole/tole-nhap.js
+++ assets/js/tole/tole-nhap.js
@@ -179,7 +179,7 @@ function findQuantityColumnIndex(headers) {
 window.addEventListener('load', () => {
   const currentUser = localStorage.getItem('currentUser');
   if (!currentUser) {
-    window.location.href = '/pages/dang_nhap.html';
+    window.location.href = 'index.html';
     return;
   }
   
@@ -192,7 +192,7 @@ window.addEventListener('load', () => {
   if (btnLogout) {
     btnLogout.addEventListener('click', () => {
       localStorage.removeItem('currentUser');
-      window.location.replace('/pages/dang_nhap.html');
+      window.location.replace('index.html');
     });
   }
   
@@ -201,7 +201,7 @@ window.addEventListener('load', () => {
   if (logo) {
     logo.style.cursor = 'pointer';
     logo.addEventListener('click', () => {
-      window.location.href = '/pages/home.html';
+      window.location.href = 'home.html';
     });
   }
   
@@ -1865,4 +1865,5 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 });
+
 
