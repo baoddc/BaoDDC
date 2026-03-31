d1707d503dee2d05bfc9c22085910a261f38e8b4
BaoDDC <thaibao06061997@gmail.com>
2026-03-31 16:42:29 +0700

    Add files via upload

--- assets/js/tole/tole-xuat.js
+++ assets/js/tole/tole-xuat.js
@@ -356,7 +356,7 @@ function findQuantityColumnIndex(headers) {
 window.addEventListener('load', () => {
   const currentUser = localStorage.getItem('currentUser');
   if (!currentUser) {
-    window.location.href = 'dang_nhap.html';
+    window.location.href = 'index.html';
     return;
   }
 
@@ -369,7 +369,7 @@ window.addEventListener('load', () => {
   if (btnLogout) {
     btnLogout.addEventListener('click', () => {
       localStorage.removeItem('currentUser');
-      window.location.replace('dang_nhap.html');
+      window.location.replace('index.html');
     });
   }
 
