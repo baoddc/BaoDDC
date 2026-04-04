647bbd97581ed05f360eeacf2fe3198157996f2a
BaoDDC <thaibao06061997@gmail.com>
2026-04-04 16:36:15 +0700

    Add files via upload

--- assets/js/tole/tole-nhap.js
+++ assets/js/tole/tole-nhap.js
@@ -373,7 +373,7 @@ window.addEventListener('load', () => {
   if (logo) {
     logo.style.cursor = 'pointer';
     logo.addEventListener('click', () => {
-      window.location.href = '/pages/home.html';
+      window.location.href = 'home.html';
     });
   }
 
--- assets/js/tole/tole-xuat.js
+++ assets/js/tole/tole-xuat.js
@@ -378,7 +378,7 @@ window.addEventListener('load', () => {
   if (logo) {
     logo.style.cursor = 'pointer';
     logo.addEventListener('click', () => {
-      window.location.href = '/pages/home.html';
+      window.location.href = 'home.html';
     });
   }
 
